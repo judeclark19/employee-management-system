3fb1c82c6407fe8d29aabc999a9af7e7135ecc42
Jude Clark <judeclark2019@gmail.com>
2020-10-05 19:39:38 -0400

    two working connections, view all emp and view all managers. Rearranging to try to get the return to main menu function to work again

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# employee-tracker
+# employee-management-system
--- custom_modules/view_functions.js
+++ custom_modules/view_functions.js
@@ -1,36 +1,54 @@
-const viewMod = [
-  {
-    viewAllEmployees: function () {
-      connection.query("SELECT * FROM employees", function (err, results) {
+import connection from "../Database/connection.js";
+
+class ViewMod {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  viewAllEmployees() {
+    connection.query("SELECT * FROM employees", function (err, results) {
+      if (err) throw err;
+      console.table(results);
+      // returnToMainMenu();
+      // return results;
+    });
+  }
+
+  viewAllManagers() {
+    connection.query(
+      "SELECT id, first_name, last_name, role_id FROM employees WHERE is_manager=1",
+      function (err, results) {
         if (err) throw err;
         console.table(results);
-      });
-    },
+        // returnToMainMenu();
+      }
+    );
+  }
+}
 
-    viewAllManagers: function () {
-      connection.query(
-        "SELECT id, first_name, last_name, role_id FROM employees WHERE is_manager=1",
-        function (err, results) {
-          if (err) throw err;
-          console.table(results);
-        }
-      );
-    },
+export default new ViewMod(connection);
 
-    viewAllDepartments: function () {
-      connection.query("SELECT * FROM departments", function (err, results) {
-        if (err) throw err;
-        console.table(results);
-      });
-    },
+//   viewAllManagers: function () {
+//     connection.query(
+//       "SELECT id, first_name, last_name, role_id FROM employees WHERE is_manager=1",
+//       function (err, results) {
+//         if (err) throw err;
+//         console.table(results);
+//       }
+//     );
+//   },
 
-    viewAllRoles: function () {
-      connection.query("SELECT * FROM roles", function (err, results) {
-        if (err) throw err;
-        console.table(results);
-      });
-    },
-  },
-];
+//   viewAllDepartments: function () {
+//     connection.query("SELECT * FROM departments", function (err, results) {
+//       if (err) throw err;
+//       console.table(results);
+//     });
+//   },
 
-export { viewMod as default };
+//   viewAllRoles: function () {
+//     connection.query("SELECT * FROM roles", function (err, results) {
+//       if (err) throw err;
+//       console.table(results);
+//     });
+//   },
+// }
